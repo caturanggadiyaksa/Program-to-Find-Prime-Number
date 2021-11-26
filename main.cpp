b2ef8793828682e5a2f21bb0c974cd9141a66317
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2021-11-26 19:34:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main()
 {
-  int num;
+  int num;  //deklarasi number
   cout << "Enter Any Number :";
-  cin >> num;
+  cin >> num; //untuk input angka
 
-  bool prime = true;
+  bool prime = true;  //tipe data yg bisa di isi salah satu nilai dari 2 nilai : true atau false
 
-  for (int i =2; i <=num/2; i++)
+  for (int i =2; i <=num/2; i++) //perulangan di mulai dari 2
   {
-    if(num%i == 0)
-      prime = false;
+    if(num%i == 0) // jika inputan angka di bagi i 0
+      prime = false; //maka prime  salah
   }
-  if(prime)
-    cout << num << "is a prime number";
+  if(prime) //jika prime true
+    cout << num << "is a prime number"; //output akan menghasilkan bilangan itu adalah prima
   else
-    cout << num << "is a not prime number";
+    cout << num << "is a not prime number"; //jika bukan prime maka output akan angka bukan bilangan prima
 
 
 
